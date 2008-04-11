a5f99a478b89c485a3b6a6995d3124d11adcad59
Chris Lamb <chris@chris-lamb.co.uk>
2008-04-11 03:10:38 +0100

    controllers/wizard.py: Tidy
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- LiveMagic/controllers/wizard.py
+++ LiveMagic/controllers/wizard.py
@@ -6,9 +6,7 @@ from DebianLive import Config, utils
 class WizardController(object):
 
     def on_wizard_apply(self, _):
-
         build_dir = utils.get_build_dir()
-
         data = self.view.get_wizard_completed_details()
 
         # Use cdebootstrap if available
@@ -16,23 +14,18 @@ class WizardController(object):
             data['bootstrap'] = 'cdebootstrap'
 
         self.model = Config(build_dir, **data)
-        self.model.save()
 
         self.view.do_dim_wizard()
-        self.do_show_build_window(self.on_wizard_build_completed)
+        self.do_show_build_window(lambda: gtk.main_quit())
 
     def on_wizard_expert_mode_selected(self, _):
         self.view.do_hide_wizard()
         self.view.do_show_main_window()
-
-    def on_wizard_build_completed(self):
-        gtk.main_quit()
 
     def get_suggested_mirror(self):
         s = utils.SourcesList()
         return s.get_mirror()
 
     def on_wizard_cancel(self, *args):
-        ret = self.view.do_show_wizard_cancel_confirm_window()
-        if ret:
+        if self.view.do_show_wizard_cancel_confirm_window():
             gtk.main_quit()

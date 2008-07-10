8f3b1b0f655dfb7df675bf7c30dcfec830b6e821
Chris Lamb <chris@chris-lamb.co.uk>
2008-07-10 22:52:31 +0100

    Search /usr/bin/ and /usr/local/bin for resources.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- LiveMagic/utils.py
+++ LiveMagic/utils.py
@@ -4,6 +4,8 @@ def find_resource(resource):
     dirs = (
         os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'misc'),
         os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+        '/usr/bin',
+        '/usr/local/bin',
         '/usr/share/live-magic',
         '/usr/local/share/live-magic',
         '/usr/share/common-licenses',
